ec5569809f8927fe2014a457bac6b5b3a43fec4e
Khuzaima <muhammedkhuzaimasheikhqureshi@gmail.com>
2026-04-29 19:15:55 -0400

    Added classes and enums

---
+++ Driver.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    public static void main(String[] args) {
+        // This is just a simple text loop
+        // It allows the program to run in a CMD or Terminal 
+        // window, satisfying the "Outside BlueJ" requirement.
+    }
+}
