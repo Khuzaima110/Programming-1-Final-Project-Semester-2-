a3d133bafcea8bcb70d5b6e574aa85336a2a058f
Khuzaima <muhammedkhuzaimasheikhqureshi@gmail.com>
2026-04-29 19:27:18 -0400

    Removed old enums since they were mistakes

--- Recipe.java
+++ Recipe.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Recipe here.
@@ -7,27 +8,7 @@
  */
 public class Recipe
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Recipe
-     */
-    public Recipe()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    private String title;
+    private ArrayList<Ingredient> ingredients;
+    
 }
